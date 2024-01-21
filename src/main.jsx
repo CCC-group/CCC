275f5e23adc65306f7adcf84ef298361e704d3da
Lacey Washington <lacey.m.washington@gmail.com>
2024-01-20 19:11:47 -0600

    added tailwind config

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+import { NextUIProvider } from "@nextui-org/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
-    <App />
-  </Router>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <NextUIProvider>
+      <Router>
+        <App />
+      </Router>
+    </NextUIProvider>
+  </React.StrictMode>
+);
