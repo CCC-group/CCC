b40292f3904e8d14e4d5adf6f8c66db56480e390
EMILY GREASON <GREASONEMILY@GMAIL.COM>
2024-01-20 22:49:37 -0800

    styled the header and started styling the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Events from "./components/Events";
 import Budgeting from "./components/Budgeting";
 import About from "./components/About";
 import Header from "./components/Header";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
