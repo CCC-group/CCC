bff07094db57609bdbd7348b6d21e58da1de0f21
Lacey Washington <lacey.m.washington@gmail.com>
2024-02-01 18:51:02 -0600

    added some stuff but nothing too crazy

--- src/components/Budgeting.jsx
+++ src/components/Budgeting.jsx
@@ -5,25 +5,78 @@ import services from "../assets/services.json";
 
 const Budgeting = () => {
   return (
-    <Grid container spacing={2}>
-      {services.map((service, idx) => (
-        <Grid item xs={12} sm={6} md={3} key={idx}>
-          <Card sx={{ border: '1px solid #ccc' }}>
-            <CardContent>
-              <img src={service.icon} alt="icon image" style={{ marginBottom: '10px' }} />
-              <CardHeader
-                title={service.service}
-                titleTypographyProps={{ align: "center" }}
-                subheaderTypographyProps={{ align: "center" }}
-              />
-              <Typography variant="body1" component="div" >
-                {service.description}
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
+    <>
+    <div style={{ textAlign: "center", marginBottom: "20px" }}>
+        <Typography>
+          <span
+            style={{
+              color: "#2243b6",
+              fontSize: "1.5em",
+              verticalAlign: "middle",
+            }}
+          >
+            &mdash;&mdash;&mdash;&mdash;&mdash;
+          </span>{" "}
+          <h2
+            style={{
+              color: "black",
+              display: "inline",
+              margin: "0 10px",
+              verticalAlign: "middle",
+            }}
+          >
+           Our Services
+          </h2>{" "}
+          <span
+            style={{
+              color: "#2243b6",
+              fontSize: "1.5em",
+              verticalAlign: "middle",
+            }}
+          >
+            &mdash;&mdash;&mdash;&mdash;&mdash;
+          </span>
+        </Typography>
+      </div>
+      <Grid container spacing={2} justifyContent="center">
+        {services.map((service, idx) => (
+          <Grid item xs={12} sm={6} md={3} key={idx}>
+            <Card sx={{ border: "1px solid #ccc", height: "100%" }}>
+              <CardContent
+                style={{
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <img
+                  src={service.icon}
+                  alt="icon image"
+                  style={{
+                    marginBottom: "10px",
+                   
+                  }}
+                />
+                <CardHeader
+                  title={service.service}
+                  titleTypographyProps={{ align: "center" }}
+                  subheaderTypographyProps={{ align: "center" }}
+                />
+                <Typography
+                  variant="body1"
+                  component="div"
+                  style={{ textAlign: "center" }}
+                >
+                  {service.description}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </>
   );
 };
 
