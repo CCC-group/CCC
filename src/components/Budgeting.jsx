344eb513fd10fdef32dd18b039be3ecb690f71e9
Lacey Washington <lacey.m.washington@gmail.com>
2024-02-04 12:28:08 -0600

    added modal to home page

--- src/components/Budgeting.jsx
+++ src/components/Budgeting.jsx
@@ -4,59 +4,98 @@ import services from "../assets/services.json";
 
 const Budgeting = () => {
   return (
-    <div className="service-body" style={{ flexGrow: 1, overflow: 'auto' }}>
-      <div style={{ textAlign: "center", padding:'15px', marginTop:'30px' }}>
+    <div className="service-body" style={{ flexGrow: 1, overflow: "auto" }}>
+      <div style={{ textAlign: "center", padding: "15px", marginTop: "30px" }}>
         <Typography>
-          <span style={{
+          <span
+            style={{
               color: "#2243b6",
               fontSize: "1.5em",
               verticalAlign: "middle",
-            }}>
+            }}
+          >
             &mdash;&mdash;&mdash;&mdash;&mdash;
           </span>{" "}
-          <h2 style={{
+          <h2
+            style={{
               color: "black",
               display: "inline",
               margin: "0 10px",
               verticalAlign: "middle",
-              fontWeight:'lighter'
-            }}>
+              fontWeight: "lighter",
+            }}
+          >
             What we do
           </h2>{" "}
-          <span style={{
+          <span
+            style={{
               color: "#2243b6",
               fontSize: "1.5em",
               verticalAlign: "middle",
-            }}>
+            }}
+          >
             &mdash;&mdash;&mdash;&mdash;&mdash;
           </span>
         </Typography>
-        <Typography variant="h1" 
+        <Typography
+          variant="h1"
           style={{
-            display:'flex', 
-            justifyContent:'center', 
-            margin: '10px', 
-            fontWeight:'bold', 
-            color: '#2243b6'
-            }}>
-              Our Services
+            display: "flex",
+            justifyContent: "center",
+            margin: "10px",
+            fontWeight: "bold",
+            color: "#2243b6",
+          }}
+        >
+          Our Services
         </Typography>
       </div>
-      <Grid container columnSpacing={0} direction="row" justifyContent="space-around">
+      <Grid
+        container
+        columnSpacing={0}
+        direction="row"
+        justifyContent="space-around"
+      >
         {services.map((service, idx) => (
-          <Grid item xs={12} sm={6} md={3} key={idx} style={{display:'flex', justifyContent:'center', textAlign:'center'}}>
-            <Card sx={{ border: '1px solid #ccc', maxWidth:'300px', marginBottom:'3px' }}>
-              <CardContent >
-                <img src={service.icon} alt="icon image" style={{ marginBottom: '5px' }} />
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={3}
+            key={idx}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              textAlign: "center",
+            }}
+          >
+            <Card
+              sx={{
+                border: "1px solid #ccc",
+                maxWidth: "300px",
+                marginBottom: "3px",
+              }}
+            >
+              <CardContent>
+                <img
+                  src={service.icon}
+                  alt="icon image"
+                  style={{ marginBottom: "5px" }}
+                />
                 <CardHeader
                   title={service.service}
                   titleTypographyProps={{ align: "center" }}
                   subheaderTypographyProps={{ align: "center" }}
                 />
-                <Typography 
-                  variant="body1" 
-                  component="div" 
-                  style={{display:'flex', justifyContent:'center',textAlign:'center'}}>
+                <Typography
+                  variant="body1"
+                  component="div"
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    textAlign: "center",
+                  }}
+                >
                   {service.description}
                 </Typography>
               </CardContent>
@@ -65,96 +104,23 @@ const Budgeting = () => {
         ))}
       </Grid>
       <div className="service-button">
-        <Button variant="contained" 
+        <Button
+          variant="contained"
           style={{
-              margin:'50px', 
-              borderRadius:'20px', 
-              fontSize: '20px', 
-              fontWeight:'lighter'}}>
+            margin: "50px",
+            borderRadius: "20px",
+            fontSize: "20px",
+            fontWeight: "lighter",
+          }}
+        >
           <Typography variant="h7" fontWeight="bold">
             To learn More <br />
-          <Typography fontWeight="light">
-              {" "}
-              Click here
-          </Typography>
+            <Typography fontWeight="light"> Click here</Typography>
           </Typography>
         </Button>
       </div>
-      <div style={{backgroundColor:'white'}}>
-        <div style={{ textAlign: "center", padding:'15px' }}>
-          <Typography>
-            <span
-              style={{
-                color: "#2243b6",
-                fontSize: "1.5em",
-                verticalAlign: "middle",
-              }}
-              >
-              &mdash;&mdash;&mdash;&mdash;&mdash;
-            </span>{" "}
-            <h2
-              style={{
-                color: "black",
-                display: "inline",
-                margin: "0 10px",
-                verticalAlign: "middle",
-                fontWeight:'lighter'
-              }}
-            >
-              Our Partners
-            </h2>{" "}
-            <span
-              style={{
-                color: "#2243b6",
-                fontSize: "1.5em",
-                verticalAlign: "middle",
-              }}
-            >
-              &mdash;&mdash;&mdash;&mdash;&mdash;
-            </span>
-          </Typography>
-          <div className="partnersContainer">
-            <img
-              className="partners"
-              src="src/assets/imgs/mutual-of-omaha-logo-blue-png.png"
-              alt="Mutual of Omaha Logo"
-            />
-            <img
-              className="partners"
-              src="src/assets/imgs/Silac.png"
-              alt="Silac Logo"
-            />
-            <img
-              className="partners"
-              src="src/assets/imgs/americanNational.png"
-              alt="American National"
-            />
-            <img
-              className="partners"
-              src="src/assets/imgs/forestersFinancialLogo.png"
-              alt="Forester Financial"
-            />
-            <img
-              className="partners"
-              src="src/assets/imgs/northAmerican.png"
-              alt="North American"
-            />
-            <img
-              className="partners"
-              src="src/assets/imgs/mediator.png"
-              alt="Mediator Debt Solutions"
-            />
-            <img
-              className="partners"
-              src="src/assets/imgs/lifeGroup.png"
-              alt="National Life Group"
-            />
-          </div>
-        </div>
-        </div>
     </div>
   );
 };
 
 export default Budgeting;
-
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,50 +1,137 @@
-import { Box, Typography } from '@mui/material'
-import logo from '../assets/imgs/CCC.jpg'
-import FmdGoodIcon from '@mui/icons-material/FmdGood';
-import CallIcon from '@mui/icons-material/Call';
-import EmailIcon from '@mui/icons-material/Email';
+import { Box, Typography } from "@mui/material";
+import logo from "../assets/imgs/CCC.jpg";
+import FmdGoodIcon from "@mui/icons-material/FmdGood";
+import CallIcon from "@mui/icons-material/Call";
+import EmailIcon from "@mui/icons-material/Email";
 
 const Footer = () => {
   return (
     <div>
-      <Box display="flex"
+      <div>
+        <div style={{ textAlign: "center", padding: "15px" }}>
+          <Typography>
+            <span
+              style={{
+                color: "#2243b6",
+                fontSize: "1.5em",
+                verticalAlign: "middle",
+              }}
+            >
+              &mdash;&mdash;&mdash;&mdash;&mdash;
+            </span>{" "}
+            <h2
+              style={{
+                color: "black",
+                display: "inline",
+                margin: "0 10px",
+                verticalAlign: "middle",
+                fontWeight: "lighter",
+              }}
+            >
+              Our Partners
+            </h2>{" "}
+            <span
+              style={{
+                color: "#2243b6",
+                fontSize: "1.5em",
+                verticalAlign: "middle",
+              }}
+            >
+              &mdash;&mdash;&mdash;&mdash;&mdash;
+            </span>
+          </Typography>
+        </div>
+        <div className="partnersContainer">
+          <img
+            className="partners"
+            src="src/assets/imgs/mutual-of-omaha-logo-blue-png.png"
+            alt="Mutual of Omaha Logo"
+          />
+          <img
+            className="partners"
+            src="src/assets/imgs/Silac.png"
+            alt="Silac Logo"
+          />
+          <img
+            className="partners"
+            src="src/assets/imgs/americanNational.png"
+            alt="American National"
+          />
+          <img
+            className="partners"
+            src="src/assets/imgs/forestersFinancialLogo.png"
+            alt="Forester Financial"
+          />
+          <img
+            className="partners"
+            src="src/assets/imgs/northAmerican.png"
+            alt="North American"
+          />
+          <img
+            className="partners"
+            src="src/assets/imgs/mediator.png"
+            alt="Mediator Debt Solutions"
+          />
+          <img
+            className="partners"
+            src="src/assets/imgs/lifeGroup.png"
+            alt="National Life Group"
+          />
+        </div>
+      </div>
+      <Box
+        display="flex"
         flexDirection="column"
         alignItems="center"
         justifyContent="center"
         height="15vh"
-        backgroundColor='#1D66C0'>
-        <div className='contact-box'>
-          <div style={{margin:'40px'}}>
+        backgroundColor="#1D66C0"
+      >
+        <div className="contact-box">
+          <div style={{ margin: "40px" }}>
             <p>
-              <FmdGoodIcon sx={{fontSize:'large', marginRight:'3px'}}/>
-              <b>Office:</b> 1S450 Summit Ave, Oakbrook Terrace, IL 60181</p>
+              <FmdGoodIcon sx={{ fontSize: "large", marginRight: "3px" }} />
+              <b>Office:</b> 1S450 Summit Ave, Oakbrook Terrace, IL 60181
+            </p>
           </div>
-          <div style={{margin:'40px'}}>
+          <div style={{ margin: "40px" }}>
             <p>
-              <CallIcon sx={{fontSize:'large', marginRight:'3px'}}/>
-              <b>Call</b> 708-232-3275</p>
+              <CallIcon sx={{ fontSize: "large", marginRight: "3px" }} />
+              <b>Call</b> 708-232-3275
+            </p>
           </div>
-          <div style={{margin:'40px'}}>
+          <div style={{ margin: "40px" }}>
             <p>
-              <EmailIcon sx={{fontSize:'large', marginRight:'3px'}}/>
-              <b>Email:</b> cworkhappen@gmail.com</p>
+              <EmailIcon sx={{ fontSize: "large", marginRight: "3px" }} />
+              <b>Email:</b> cworkhappen@gmail.com
+            </p>
           </div>
         </div>
       </Box>
       <div className="footer">
         <a className="navbar-brand" href="/">
-          <img src={logo} 
-            alt="Logo" 
-            width="120" 
-            height="114" 
-            className="d-inline-block align-text-top"/>
+          <img
+            src={logo}
+            alt="Logo"
+            width="120"
+            height="114"
+            className="d-inline-block align-text-top"
+          />
         </a>
-      <Typography variant='h7' 
-        style={{color:'white', fontFamily:'system-ui, -apple-system, BlinkMacSystemFont, Segoe UI', padding:'10px'}}>
-          &copy;2024 All Rights Reserved</Typography>
+        <Typography
+          variant="h7"
+          style={{
+            color: "white",
+            fontFamily:
+              "system-ui, -apple-system, BlinkMacSystemFont, Segoe UI",
+            padding: "10px",
+          }}
+        >
+          &copy;2024 All Rights Reserved
+        </Typography>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
